6f1d2d4c9bbdb50fec1870c25ad57d88c5d4a350
ManBand <alapchev@yandex.ru>
2024-10-19 22:19:05 +0300

    Initial Commit

---
+++ Traffic Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Light
+//
+//  Created by Пупка on 19.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
